7a989ca186b87c3714d7993c7382aa3d029b52bf
FX-Wood <fxwood2@gmail.com>
2019-04-02 14:35:36 -0700

    Adding comments

--- stack.test.py
+++ stack.test.py
@@ -2,5 +2,18 @@ from stack import Stack
 
 print(Stack)
 
+stack = Stack()
 
+# add two ints to the stack
+stack.push(5)
+stack.push(13)
 
+print( stack.peek() ) # prints 5
+
+stack.pop() # returns 5
+
+print( stack.peek() ) # prints 13
+
+# these lines will empty the stack and return an index error
+# stack.pop()
+# print( stack.peek() )
