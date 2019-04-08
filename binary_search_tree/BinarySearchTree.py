657da50c452f9ff2c114e64147263f0dac1a53fe
FX-Wood <fxwood2@gmail.com>
2019-04-08 15:39:08 -0700

    Updating for base case

--- binary_search_tree/BinarySearchTree.py
+++ binary_search_tree/BinarySearchTree.py
@@ -33,8 +33,10 @@ class BinarySearchTree():
                 node.right = self.insert(data, node)
             return node
     
-    def in_order(self, node):
+    def in_order(self, node=None):
         # left, root, right
+        if not node:
+            node = self.root
         if node:
             if node.left:
                 self.in_order(node.left)
@@ -44,8 +46,10 @@ class BinarySearchTree():
             if node.right:
                 self.in_order(node.right)
 
-    def pre_order(self, node):
+    def pre_order(self, node=None):
         # root, left, right
+        if not node:
+            node = self.root
         if node:
             print(node.data)
             if node.left:
@@ -53,8 +57,10 @@ class BinarySearchTree():
             if node.right:
                 self.pre_order(node.right)
 
-    def post_order(self, node):
+    def post_order(self, node=None):
         # left, right, root
+        if not node:
+            node = self.root
         if node:
             if node.left:
                 self.post_order(node)
